17f5c6f794f9754a9f2aa07cc57cf2ada0d50590
Shane Bramble-Wade <55419571+shanebramble@users.noreply.github.com>
2020-12-08 10:15:55 -0500

    Updated the logic

--- front_end/Login&Sign/js/login.js
+++ front_end/Login&Sign/js/login.js
@@ -1,6 +1,5 @@
-
-const login-username = document.getElementById("userNameLogin");
-const login-password = document.getElementById("passwordLogin");
+const login_username = document.getElementById("userNameLogin");
+const login_password = document.getElementById("passwordLogin");
 const url = "http://localhost:8080";
 
 
@@ -9,28 +8,28 @@ const login = (event) => {
   event.preventDefault();
 
   fetch(`${url}/api/login`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "aplication/x-www-form-urlencoded",
-    },
-    body: JSON.stringify({
-      username: login-username.value,
-      password: login-password.value,
-    }),
-  })
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "aplication/x-www-form-urlencoded",
+      },
+      body: JSON.stringify({
+        username: login_username.value,
+        password: login_password.value,
+      }),
+    })
     .then((res) => res.json())
     .then((res) => {
 
-             if (res.success & res.token) {
-              localstorage.setItem('token', res.token)
-            }
-            else if (res.failure || res.status === "wrong user or pass") {
-              console.log("error", res.status);
+      if (res.success & res.token) {
+        localstorage.setItem('token', res.token)
+        window.location.href = "./main.html";
+      } else if (res.failure || res.status === "wrong user or pass") {
+        console.log("error", res.status);
 
-            }
-          })
-          .catch((err) => console.log("error occurred", err));
+      }
+    })
+    .catch((err) => console.log("error occurred", err));
 };
 
 document.getElementById("login-form").addEventListener("submit", login);
--- front_end/Login&Sign/js/register.js
+++ front_end/Login&Sign/js/register.js
@@ -1,46 +1,46 @@
 const myForm = document.getElementById("registration-form")
-  const firstname = document.getElementById("firstname");
-  const lastname = document.getElementById("lastname");
-  const email = document.getElementById("email");
-  const username = document.getElementById("username");
-  const mobile = document.getElementById("phone-no");
-  const password = document.getElementById("password");
-  // const confirm_password = document.getElementById('confirm_password')
-  const url = "http://localhost:8080";
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const email = document.getElementById("email");
+const username = document.getElementById("username");
+const mobile = document.getElementById("phone-no");
+const password = document.getElementById("password");
+// const confirm_password = document.getElementById('confirm_password')
+const url = "http://localhost:8080";
 
-const submitForm = (event)=> {
-     event.preventDefault()
+const submitForm = (event) => {
+  event.preventDefault()
 
- fetch(`${url}/api/createuser`, {
-   // mode: "no-cors",
-   method: "POST",
-   headers: {
-     Accept: "application/json, text/plain, */*",
-     "content-type": "aplication/x-www-form-urlencoded",
-   },
-   // credentials: "include",
-   body: JSON.stringify({
-     first_name: firstname.value,
-     last_name: lastname.value,
-     email: email.value,
-     username: username.value,
-     phoneNumber: mobile.value,
-     password: password.value,
-   }),
- })
-   .then((res) => res.json())
-   .then((res) => {
-                if (res.success & res.token) {
-             alert('user created successfully')
-             localstorage.setItem('token', res.token)
-             localStorage.setItem("user", res.data);
-             window.location.href = "./profile.html";
-           } else if (res.status === "invalid username") {
-             console.log("error", res.status);
+  fetch(`${url}/api/createuser`, {
+      // mode: "no-cors",
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "content-type": "aplication/x-www-form-urlencoded",
+      },
+      // credentials: "include",
+      body: JSON.stringify({
+        first_name: firstname.value,
+        last_name: lastname.value,
+        email: email.value,
+        username: username.value,
+        phoneNumber: mobile.value,
+        password: password.value,
+      }),
+    })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success & res.token) {
+        alert('user created successfully')
+        localstorage.setItem('token', res.token)
+        localStorage.setItem("user", res.data);
+        window.location.href = "./main.html";
+      } else if (res.status === "invalid username") {
+        console.log("error", res.status);
 
-           }
-         })
-         .catch((err) => console.log("error occurred", err));
+      }
+    })
+    .catch((err) => console.log("error occurred", err));
 
 };
 
