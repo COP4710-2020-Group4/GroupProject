733e9834962ef0f79113b8d6ab2a9fb0acb9400b
grizzlyThresher <grizzlyThresher@gmail.com>
2020-12-08 16:17:45 -0500

    Login can now properly call the API

--- front_end/Login&Sign/js/login.js
+++ front_end/Login&Sign/js/login.js
@@ -1,35 +1,34 @@
 const login_username = document.getElementById("userNameLogin");
 const login_password = document.getElementById("passwordLogin");
-const url = "http://localhost:8080";
+const login_url = "http://localhost:8080";
 
 
 
 const login = (event) => {
-  event.preventDefault();
+  //console.log("Logging In");
+  let options = {
+          method: "POST",
+          headers: {
+              Accept: "application/json, x-www-form-urlencoded, text/plain, */*",
+              "Content-Type": "aplication/x-www-form-urlencoded",
+          },
+          body: JSON.stringify({
+              username: login_username.value,
+              password: login_password.value,
+          })};
+  //console.log(options);
 
-  fetch(`${url}/api/login`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "aplication/x-www-form-urlencoded",
-      },
-      body: JSON.stringify({
-        username: login_username.value,
-        password: login_password.value,
-      }),
-    })
+  fetch(`${login_url}/api/login`, options)
     .then((res) => res.json())
     .then((res) => {
-
+      //console.log(res);
       if (res.success & res.token) {
         localstorage.setItem('token', res.token)
         window.location.href = "./main.html";
-      } else if (res.failure || res.status === "wrong user or pass") {
+      } else if (!res.success || res.status === "Wrong user or pass") {
         console.log("error", res.status);
 
       }
     })
     .catch((err) => console.log("error occurred", err));
 };
-
-document.getElementById("login-form").addEventListener("submit", login);
