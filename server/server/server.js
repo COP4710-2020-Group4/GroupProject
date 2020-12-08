065a481a4cc3d525bbe3bdba715b95f45b0c5b00
Maurizio Brancato <mbrancato31@gmail.com>
2020-12-08 14:48:11 -0500

    fix server cors

--- server/server/server.js
+++ server/server/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const apiRouter = require('./routes');
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
