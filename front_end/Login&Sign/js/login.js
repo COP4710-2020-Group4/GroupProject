8d149c1fbca9cb79f269b5d8acdc2bab5a3ea78e
grizzlyThresher <grizzlyThresher@gmail.com>
2020-12-08 16:55:49 -0500

    Login Works

--- front_end/Login&Sign/js/login.js
+++ front_end/Login&Sign/js/login.js
@@ -5,30 +5,32 @@ const login_url = "http://localhost:8080";
 
 
 const login = (event) => {
-  //console.log("Logging In");
   let options = {
           method: "POST",
           headers: {
-              Accept: "application/json, x-www-form-urlencoded, text/plain, */*",
-              "Content-Type": "aplication/x-www-form-urlencoded",
+              "Content-Type": "application/json",
           },
           body: JSON.stringify({
               username: login_username.value,
               password: login_password.value,
           })};
-  //console.log(options);
 
   fetch(`${login_url}/api/login`, options)
     .then((res) => res.json())
     .then((res) => {
-      //console.log(res);
-      if (res.success & res.token) {
-        localstorage.setItem('token', res.token)
+      if (res.status === "success" && res.token != null) {
+        window.localStorage.setItem('token', res.token);
         window.location.href = "./main.html";
-      } else if (!res.success || res.status === "Wrong user or pass") {
+        return true;
+      } else if (res.status === "Wrong user or pass") {
         console.log("error", res.status);
-
+        alert("Invalid Username or Password.");
+        return false;
       }
     })
-    .catch((err) => console.log("error occurred", err));
+    .catch((err) => {
+        console.log("error occurred", err);
+        alert("Error occurred.");
+        return false;
+    });
 };
