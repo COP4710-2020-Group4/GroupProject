fc9480bbd21c76ddcd4ad440aa0ae090dbb3837a
Maurizio Brancato <36704748+mbrancato31@users.noreply.github.com>
2020-12-07 20:16:09 -0500

    updated bd

--- database/DB_Project.sql
+++ database/DB_Project.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `db_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_project`;
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
 --
 -- Host: localhost    Database: db_project
 -- ------------------------------------------------------
--- Server version	8.0.22
+-- Server version	8.0.18
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,12 +26,12 @@ DROP TABLE IF EXISTS `attends`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `attends` (
   `comment` tinytext,
-  `rating` int DEFAULT NULL,
-  `userID` int DEFAULT NULL,
-  `eventID` int DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `eventID` int(11) DEFAULT NULL,
   KEY `userID` (`userID`),
   KEY `eventID` (`eventID`),
-  CONSTRAINT `attends_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`),
+  CONSTRAINT `attends_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `attends_ibfk_2` FOREIGN KEY (`eventID`) REFERENCES `event` (`eventID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -54,14 +56,14 @@ DROP TABLE IF EXISTS `event`;
 CREATE TABLE `event` (
   `category` tinytext,
   `date` varchar(200) DEFAULT NULL,
-  `eventID` int NOT NULL,
+  `eventID` int(11) NOT NULL,
   `description` tinytext,
   `event_Name` varchar(200) DEFAULT NULL,
   `address` varchar(100) DEFAULT NULL,
-  `userID` int DEFAULT NULL,
+  `userID` int(11) DEFAULT NULL,
   PRIMARY KEY (`eventID`),
   KEY `userID` (`userID`),
-  CONSTRAINT `event_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
+  CONSTRAINT `event_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -71,7 +73,7 @@ CREATE TABLE `event` (
 
 LOCK TABLES `event` WRITE;
 /*!40000 ALTER TABLE `event` DISABLE KEYS */;
-INSERT INTO `event` VALUES ('example','2020-12-01',1,'example','Example Event',NULL,NULL);
+INSERT INTO `event` VALUES ('example','2020-12-01',1,'example','Example Event',NULL,NULL),('concert','2020-12-03',83227,'another concert','a concert','33346 example rd, hialeah,florida',2),('concert','2020-12-04',95220,'another concert','a concert','33346 example rd, hialeah,florida',3);
 /*!40000 ALTER TABLE `event` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,11 +85,11 @@ DROP TABLE IF EXISTS `superadmin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `superadmin` (
-  `userID` int NOT NULL AUTO_INCREMENT,
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   `token` varchar(100) DEFAULT NULL,
-  `isAdmin` tinyint DEFAULT NULL,
+  `isAdmin` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`userID`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `token_UNIQUE` (`token`),
@@ -101,7 +103,7 @@ CREATE TABLE `superadmin` (
 
 LOCK TABLES `superadmin` WRITE;
 /*!40000 ALTER TABLE `superadmin` DISABLE KEYS */;
-INSERT INTO `superadmin` VALUES (1,'mau','$2b$10$QBn4hbhutvsFZi6gUwiUNOjgw9c7rS.AAnVyXc6f7dLyHFgIMdk8a','53858764412020110418',1);
+INSERT INTO `superadmin` VALUES (1,'mau','$2b$10$QBn4hbhutvsFZi6gUwiUNOjgw9c7rS.AAnVyXc6f7dLyHFgIMdk8a','11773323752020110517',1);
 /*!40000 ALTER TABLE `superadmin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -113,7 +115,7 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `userID` int NOT NULL AUTO_INCREMENT,
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   `token` varchar(100) DEFAULT NULL,
@@ -121,11 +123,11 @@ CREATE TABLE `users` (
   `first_name` varchar(100) DEFAULT NULL,
   `last_name` varchar(100) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `phoneNumber` bigint DEFAULT NULL,
+  `phoneNumber` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`userID`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `UQ_userToken` (`token`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -134,7 +136,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'example','$2b$10$o81A1Qo2yh6x4BMpgsoK.e.x3qOHeyrlAdH6qcMPrz7nalv2G0Rha','80377771532020110418',0,'ex','ample','m@mail.com',7774747777);
+INSERT INTO `users` VALUES (1,'example','$2b$10$o81A1Qo2yh6x4BMpgsoK.e.x3qOHeyrlAdH6qcMPrz7nalv2G0Rha','46024369302020110721',0,'ex','ample','m@mail.com',7774747777),(2,'example2','$2b$10$o3ln8MDJviJla5sLCyEl7.sc/dUVrFupRBpK6ho3OvvneqEZq1X2O','93318636542020110518',1,'m','b','m@com',2222222222),(3,'maurizio','$2b$10$izOwq9nDJGJW4qkASOwvG.pBfxjO4adREeXh8sRPXfWKuc4b.atky','73804785052020110518',1,'ma','ba','ma@coma',2222222223);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -155,4 +157,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-05 15:33:05
+-- Dump completed on 2020-12-07 20:14:57
